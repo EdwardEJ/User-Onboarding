4145c98ff4a203b94af100964640a65c405a14db
A <jesus.e.estrada@hotmail.com>
2020-08-19 18:30:41 -0500

    added initial states

--- user-onboarding/src/components/App.js
+++ user-onboarding/src/components/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Form from './Form'
+
+import formSchema from '../validation/formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
 
 import '../App.css';
 
+const initialFormValues = {
+  Name: '',
+  Email: '',
+  Password: '',
+  Terms: '' //(checkbox)
+};
+
+const initialFormErrors = {
+  Name: '',
+  Email: '',
+  Password: '',
+  Terms: '' //(checkbox)
+};
+
+const initialUsers = [{ Name: 'ed' }];
+const initialDisable = true;
+
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisable)
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <Form />
+
         {`hello world`}
+
+        {
+          users.map(user => {
+            return (
+              <div key={user.id}>
+                <h2>{user.Name}</h2>
+                <p>{user.Email}</p>
+              </div>
+            )
+          })
+        }
       </header>
     </div>
+
+
   );
 }
 
